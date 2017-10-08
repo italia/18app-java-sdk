32972300ac9f9f26d7b11d8f645d03bd720869ed
Daniele <d.agosti08@gmail.com>
2017-10-08 11:39:35 +0200

    test classes

--- src/java/test/TestConsume.java
+++ src/java/test/TestConsume.java
@@ -10,23 +10,15 @@ public class TestConsume {
 
     public static void main(String[] args) {
 
-        String nomeCliente = "Attivazione effettuata";
         String codVoucher = "iThHnbGY";
-        String keystorePath = "";
-        String password = "";
+
         try {
 
             MerchantService service = new MerchantService("AAAAAA00H01H501P.p12", "m3D0T4aM");
-            CheckResponse checkResponse = service.activateCertificate();
             // Does not consume the voucher and does not scale the amount from the purse
             // The merchant is now active
 
-            if (!checkResponse.getNominativoBeneficiario().equals(nomeCliente)){
-                System.out.println("nome del cliente non corrisponde con il noominativo del beneficiario del voucher");
-                return;
-            }
-
-            checkResponse = service.checkAndConsumeOperation(codVoucher);
+            CheckResponse checkResponse = service.checkAndConsumeOperation(codVoucher);
 
             System.out.println(checkResponse);
 
--- src/java/test/TestFreezeAndConfirm.java
+++ src/java/test/TestFreezeAndConfirm.java
@@ -10,23 +10,15 @@ public class TestFreezeAndConfirm {
 
     public static void main(String[] args) {
 
-        String nomeCliente = "Attivazione effettuata";
         String codVoucher = "Zirfsz0d";
-        String keystorePath = "";
-        String password = "";
+
         try {
 
             MerchantService service = new MerchantService("AAAAAA00H01H501P.p12", "m3D0T4aM");
-            CheckResponse checkResponse = service.activateCertificate();
             // Does not consume the voucher and does not scale the amount from the purse
             // The merchant is now active
 
-            if (!checkResponse.getNominativoBeneficiario().equals(nomeCliente)) {
-                System.out.println("nome del cliente non corrisponde con il noominativo del beneficiario del voucher");
-                return;
-            }
-
-            checkResponse = service.checkAndFreezeOperation(codVoucher);
+            CheckResponse checkResponse = service.checkAndFreezeOperation(codVoucher);
             System.out.println(checkResponse);
 
             ConfirmResponse confirmResponse = service.confirmOperation(codVoucher, checkResponse.getImporto());
