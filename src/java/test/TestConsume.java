f66e54a5cff11a2b7bb91983eeeccf8df6b1d453
Daniele <d.agosti08@gmail.com>
2017-10-08 11:23:04 +0200

    test classes

--- src/java/test/TestConsume.java
+++ src/java/test/TestConsume.java
@@ -10,18 +10,18 @@ public class TestConsume {
 
     public static void main(String[] args) {
 
-        String nomeCliente = "";
-        String codVoucher = "";
+        String nomeCliente = "Attivazione effettuata";
+        String codVoucher = "iThHnbGY";
         String keystorePath = "";
         String password = "";
         try {
 
-            MerchantService service = new MerchantService(keystorePath, password);
+            MerchantService service = new MerchantService("AAAAAA00H01H501P.p12", "m3D0T4aM");
             CheckResponse checkResponse = service.activateCertificate();
             // Non consuma il voucher e non scala l'importo dal borsellino
             // Adesso l'esercente è attivo
 
-            if (checkResponse.getNominativoBeneficiario() != nomeCliente){
+            if (!checkResponse.getNominativoBeneficiario().equals(nomeCliente)){
                 System.out.println("nome del cliente non corrisponde con il noominativo del beneficiario del voucher");
                 return;
             }
